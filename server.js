842853ac59a2826390d12f0b15768e04a608d26d
arun1705 <arun.hosamani@nelito.com>
2019-02-06 10:33:40 +0530

    posting with arc

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 app.post('/', (req, res) => {
     var email = req.body.email;
     var amount = req.body.amount
 
-    res.send({ "email": email, "amount": amount });
+    res.send({
+        "email": email,
+        "amount": amount
+    });
 })
 
 app.listen(3000, () => {
