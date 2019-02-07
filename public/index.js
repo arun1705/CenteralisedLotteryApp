e39d286884737e40f075fa5fe46153bd4155dc25
arun1705 <arun.hosamani@nelito.com>
2019-02-07 12:18:53 +0530

    react setup

---
+++ public/index.js
@@ -0,0 +1,15 @@
+class App extends React.Component{
+    render(){
+        return(
+        <div>
+         <h1>LOTTERY APPLICATION!</h1> 
+        </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <div>
+      <App />
+    </div>
+, document.getElementById('reactBinding'));
