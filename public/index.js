2715945a593300f0340b7a6f2d870353596c05b7
arun1705 <arun.hosamani@nelito.com>
2019-02-07 12:29:15 +0530

    adding fields to view

--- public/index.js
+++ public/index.js
@@ -1,8 +1,23 @@
 class App extends React.Component{
+constructor(props){
+    super(props);
+    this.state={
+        "total_amount" :1000
+    }
+}
+
     render(){
         return(
         <div>
-         <h1>LOTTERY APPLICATION!</h1> 
+         <h1>LOTTERY APPLICATION!</h1>
+         <div>
+             <p>Total lottery amount is {this.state.total_amount}</p>
+         </div> 
+         <form>
+             <input placeholder="amount"/>
+             <input placeholder="email"/>
+             <button>Participate</button>
+         </form>
         </div>
         )
     }
